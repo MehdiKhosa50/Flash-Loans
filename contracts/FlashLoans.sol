8cf1d9f54aa1970fc34b41ffbccfced31a23ccd7
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-09-09 20:04:20 +0500

    FlashLoan Contract

---
+++ contracts/FlashLoans.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19;
+
+contract FlashLoans {
+    // Uniswap interface and library imports
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IERC20.sol";
+import "./libraries/UniswapV2Library.sol";
+import "./libraries/SafeERC20.sol";
+import "hardhat/console.sol";
+}
